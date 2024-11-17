7ca271a2417209c88ef5f6a3d5965148e8d41e31
Ben_Sheeley <sheeleyb@grinnell.edu>
2024-11-17 14:34:36 -0600

    lab

--- src/main/java/edu/grinnell/csc207/blocks/Surrounded.java
+++ src/main/java/edu/grinnell/csc207/blocks/Surrounded.java
@@ -4,7 +4,7 @@ package edu.grinnell.csc207.blocks;
  * A text block surrounded by a single letter.
  *
  * @author Samuel A. Rebelsky
- * @author Your Name Here
+ * @author Benjamin Sheeley
  */
 public class Surrounded implements AsciiBlock {
   // +--------+------------------------------------------------------------
